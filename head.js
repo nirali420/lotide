ea0f554faaa9c8180298ac74ee0a1a620483e2bd
Nirali <nirali420@gmail.com>
2019-10-21 16:57:40 +0000

    Refactor the files to add assertEqual function

--- head.js
+++ head.js
@@ -1,15 +1,14 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log(`Assertion Passed: ${actual} === ${expected}`);
-    } else {
-        console.log(`Assertion Failed: ${actual} !== ${expected}`);
-    }
-};
+// const assertEqual = function (actual, expected) {
+// 	if (actual === expected) {
+// 		console.log(`Assertion Passed: ${actual} === ${expected}`);
+// 	} else {
+// 		console.log(`Assertion Failed: ${actual} !== ${expected}`);
+// 	}
+// };
+
 const head = function(array) {
-    return array[0]
+	return array[0]
 };
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
